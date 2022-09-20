eb074177b1e7709e3ffd6bdc52cb0ec6e3296108
Adeola23 <amosadeola92@gmail.com>
2022-09-20 12:18:11 +0100

    updated url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a5fd5342df67b43c790266cedba2b299-572176145.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
